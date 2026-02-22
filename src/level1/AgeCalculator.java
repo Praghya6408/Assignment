c20fdae8d43550cf10e91836538a87350f546ec1
Praghya <pg6408@srmist.edu.in>
2026-02-23 00:44:47 +0530

    Add level1 programs

---
+++ src/level1/AgeCalculator.java
@@ -0,0 +1,15 @@
+// Program to calculate Harry's age
+
+public class AgeCalculator {
+
+    public static void main(String[] args) {
+
+        int birthYear = 2000;
+        int currentYear = 2024;
+
+        int age = currentYear - birthYear;
+
+        System.out.println("Harry's age in " + currentYear + " is " + age);
+
+    }
+}
